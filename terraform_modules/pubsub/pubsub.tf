e3022602931486a4b2a991d539ae8409f98d5b17
Tein de Vries <tdevries@xccelerated.io>
2020-07-23 17:51:50 +0200

    cleaned code (minor)

--- terraform_modules/pubsub/pubsub.tf
+++ terraform_modules/pubsub/pubsub.tf
@@ -1,8 +1,7 @@
 ##### Pub/Sub #####
 
 resource "google_project_service" "pubsub-service" {
-  project = var.project-id
-  service = "pubsub.googleapis.com"
-  # disable_dependent_services = true
+  project            = var.project-id
+  service            = "pubsub.googleapis.com"
   disable_on_destroy = false
 }
--- terraform_modules/pubsub/request-topic.tf
+++ terraform_modules/pubsub/request-topic.tf
@@ -9,16 +9,13 @@ resource "google_pubsub_topic" "topic" {
 
 
 ##### publishers #####
+
 # would it be better to make policies out of these?
 resource "google_pubsub_topic_iam_member" "publisher" {
-  #TODO replace below with service account of Cloud Run
   member     = "serviceAccount:${var.api-email}"
   topic      = "projects/${var.project-id}/topics/${var.topic["name"]}"
   role       = "roles/pubsub.publisher"
   depends_on = [google_pubsub_topic.topic]
-  # # for multiple:
-  # count      = length(var.topic["publisher"])
-  # member     = "serviceAccount:${element(var.topic["publisher"], count.index)}@${var.project-id}.iam.gserviceaccount.com"
 }
 
 
@@ -31,10 +28,6 @@ resource "google_pubsub_subscription_iam_binding" "subscriber" {
   ]
   role       = "roles/pubsub.subscriber"
   depends_on = [google_pubsub_subscription.subscription]
-  # # for multiple:
-  # subscription = google_pubsub_subscription.subscription[count.index].name
-  # count        = length(google_pubsub_subscription.subscription)
-  # members      = [for _, subscriber in var.topic["subscriptions"][google_pubsub_subscription.subscription[count.index].name] : "serviceAccount:${subscriber}@${var.project-id}.iam.gserviceaccount.com"]
 }
 
 resource "google_pubsub_subscription" "subscription" {
@@ -42,7 +35,4 @@ resource "google_pubsub_subscription" "subscription" {
   topic      = "projects/${var.project-id}/topics/${var.topic["name"]}"
   project    = var.project-id
   depends_on = [google_pubsub_topic.topic]
-  # # for multiple:
-  # count = length(keys(var.topic["subscriptions"]))
-  # name  = element(keys(var.topic["subscriptions"]), count.index)
 }
